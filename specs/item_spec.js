f20ae1b3ded494e36facfa3ecf4cc99aa26dc0ae
Tom Benham <benji26@hotmail.co.uk>
2016-11-30 08:13:07 +0000

    deleted item files

--- specs/item_spec.js
+++
@@ -1,8 +0,0 @@
-// var items = require('../item');
-// var assert = require('assert');
-
-// describe( "items", function() {
-//   it("items has a count", function() {
-//     assert.equal(4, items.length);
-//      })
-// });
